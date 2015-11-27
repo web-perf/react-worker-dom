2f512521734d6c5cce45d4810f5b9dd2d2b016b4
Parashuram <code@nparashuram.com>
2015-11-27 12:10:09 -0800

    Updated package.json, exported main file

--- test/app/main.jsx
+++ test/app/main.jsx
@@ -5,6 +5,6 @@ import Com from './testApp.jsx'
 // If this is a regular ReactJS app, just load the react-dom for rendering, else if it is in a worker, use react-worker-dom
 
 onmessage = (e => {
-    const reactDom = require(e.data.isWorker ? './../../src/worker' : 'react-dom');
+    const reactDom = require(e.data.isWorker ? './../../' : 'react-dom');
     reactDom.render(<DBMon/>, e.data.el);
 });
