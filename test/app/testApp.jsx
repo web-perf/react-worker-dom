3a2c3bb67de9201519d27a298d9d3604a014aab3
Parashuram <code@nparashuram.com>
2015-11-28 10:58:32 -0800

    Added a sample TODO app with events for testing

--- test/app/testApp.jsx
+++ test/app/testApp.jsx
@@ -1,43 +1,39 @@
 import React from 'react';
 
-class Com11 extends React.Component {
-    render() {
-        return <com11>Com11</com11>
+var TodoList = React.createClass({
+  render: function() {
+    var createItem = function(itemText, index) {
+      return <li key={index + itemText}>{itemText}</li>;
     };
-}
-class Com12 extends React.Component {
-    render() {
-        return <com12>Com12</com12>
-    };
-}
-class Com2 extends React.Component {
-    render() {
-        return <com2>Com2</com2>
-    };
-}
-class Com1 extends React.Component {
-    render() {
-        return <com1><h2>Com1</h2><Com11/><Com12/></com1>
-    };
-}
-export default class Com extends React.Component {
-      constructor(p){
-        super(p);
-        this.state = {secondsElapsed:0};
-      }
-      tick() {
-        this.setState({secondsElapsed: this.state.secondsElapsed + 1});
-      }
-      componentDidMount() {
-        this.interval = setInterval(this.tick.bind(this), 100);
-      }
-      componentWillUnmount(){
-        clearInterval(this.interval);
-      }
-      render(){
-        return (
-          <div>Seconds Elapsed: {this.state.secondsElapsed}</div>
-        );
-      }
-}
+    return <ul>{this.props.items.map(createItem)}</ul>;
+  }
+});
 
+var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {items: [], text: ''};
+  },
+  onChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var nextItems = this.state.items.concat([this.state.text]);
+    var nextText = '';
+    this.setState({items: nextItems, text: nextText});
+  },
+  render: function() {
+    return (
+      <div>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.onChange} value={this.state.text} />
+          <button>{'Add #' + (this.state.items.length + 1)}</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+export default TodoApp;
