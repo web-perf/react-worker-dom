ccba224bf570ae0ecffdd787cd179df48bc02fb7
Parashuram <code@nparashuram.com>
2016-01-16 15:45:18 -0800

    Moved logic to create containers to index.html

--- test/dbmonster/main-normal.jsx
+++ test/dbmonster/main-normal.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 import reactDOM from 'react-dom';
 
 import DBMon from './components/app.jsx';
-//import DBMon from './testApp.jsx'
 
-var content = document.getElementById('content');
 for (var i = 0; i < ENV.count; i++) {
-	var container = document.createElement('div');
-	container.style.width = 100 / ENV.count + '%';
-	content.appendChild(container);
-
-	reactDOM.render(<DBMon rows={ENV.rows} timeout={ENV.timeout}/>, container);
+	reactDOM.render(<DBMon rows={ENV.rows} timeout={ENV.timeout}/>, document.getElementById('topLevelContainer-' + i));
 }
--- test/dbmonster/main-worker.js
+++ test/dbmonster/main-worker.js
@@ -1,11 +1,5 @@
-
-var content = document.getElementById('content');
 for (var i = 0; i < ENV.count; i++) {
-	var container = document.createElement('div');
-	container.style.width = 100 / ENV.count + '%';
-	content.appendChild(container);
-
-	var worker = new Worker('worker-impl.js');
-	worker.postMessage(ENV);
-	new ReactWorker(worker, container);
+    var worker = new Worker('worker-impl.js');
+    worker.postMessage(ENV);
+    new ReactWorker(worker, document.getElementById('topLevelContainer-' + i));
 }
