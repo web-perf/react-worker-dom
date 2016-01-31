a749b1d059d101bb9eba1210d5c9fbd456d2e498
Parashuram <code@nparashuram.com>
2016-01-30 19:49:03 -0800

    Moved NodeList on page to external class

--- src/page/index.js
+++ src/page/index.js
@@ -1,15 +1,15 @@
 import WorkerDomNodeImpl from './WorkerDomNodeImpl';
 import Channel from './../common/channel';
-import {EVENT, RENDER_TIME, ADD_EVENT_HANDLERS, REMOVE_CHILD, REMOVE_EVENT_HANDLERS, RENDER_QUEUE, CONSTRUCTOR, ADD_CHILD, RENDER, SET_ATTRIBUTES, SET_CONTENT} from './../common/constants';
+import { EVENT, RENDER_TIME, ADD_EVENT_HANDLERS, REMOVE_CHILD, REMOVE_EVENT_HANDLERS, RENDER_QUEUE, CONSTRUCTOR, ADD_CHILD, RENDER, SET_ATTRIBUTES, SET_CONTENT } from './../common/constants';
 import ReactMount from 'react/lib/ReactMount';
 
 import inject from './inject';
+import NodeIDOps from './NodeIDOperations';
 
 class ReactWorkerDom {
     constructor(worker, container) {
         inject();
 
-        this.nodeList = {};
         this.container = container;
 
         this.channel = new Channel(worker);
@@ -21,60 +21,63 @@ class ReactWorkerDom {
     }
 
     handleMessage(data) {
-        switch (data.type){
+        switch (data.type) {
             case RENDER_QUEUE:
                 var start = performance.now();
                 data.args.forEach(msg => this.handleRenderQueueMessage(msg));
-                this.channel.send(RENDER_TIME,{
+                this.channel.send(RENDER_TIME, {
                     time: performance.now() - start,
                     count: data.args.length
                 });
                 break;
-            default: 
+            default:
                 console.log('Cannot handle message %s', data.type, data.args);
         }
     }
 
     handleRenderQueueMessage(data) {
-        var nodeList = this.nodeList;
+        var node;
+        if (data.method !== CONSTRUCTOR) {
+            node = NodeIDOps.get(data.guid);
+        }
+
         switch (data.method) {
             case CONSTRUCTOR:
-                nodeList[data.guid] = new WorkerDomNodeImpl(data.guid, data.reactId, ...data.args);
+                node = new WorkerDomNodeImpl(data.guid, data.reactId, ...data.args);
+                NodeIDOps.add(node);
                 break;
             case RENDER: // Should only be called once per worker
-                this.container.appendChild(nodeList[data.guid].ref);
-                ReactMount.registerContainer(nodeList[data.guid].ref);
+                this.container.appendChild(node.ref);
+                ReactMount.registerContainer(node.ref);
                 break;
             case ADD_CHILD:
-                var node = nodeList[data.guid];
-                node.addChild(nodeList[data.args[0]]);
+                node.addChild(NodeIDOps.get(data.args[0]));
                 break;
             case REMOVE_CHILD:
-                var node = nodeList[data.guid];
-                node.removeChild(nodeList[data.args[0]]);
+                node.removeChild(NodeIDOps.get(data.args[0]));
                 break;
             case SET_ATTRIBUTES:
-                nodeList[data.guid].setAttributes(...data.args);
+                node.setAttributes(...data.args);
                 break;
             case SET_CONTENT:
-                nodeList[data.guid].setContent(...data.args);
+                node.setContent(...data.args);
                 break;
             case ADD_EVENT_HANDLERS:
-                nodeList[data.guid].addEventHandlers(this.container, this.onEvent.bind(this), ...data.args);
+                node.addEventHandlers(this.container, this.onEvent.bind(this), ...data.args);
                 break;
             case REMOVE_EVENT_HANDLERS:
-                nodeList[data.guid].removeEventHandlers();
+                node.removeEventHandlers();
                 break;
             default:
                 console.log('Cannot run %s on Node with id %s', data.method, data.id);
         }
     }
 
-    onEvent(handler, syntheticEvent, reactId, e){
+    onEvent(handler, syntheticEvent, reactId, e) {
         this.channel.send(EVENT, {
             reactId,
             eventType: handler,
-            event: Channel.serializeEvent(syntheticEvent)
+                event: Channel.serializeEvent(syntheticEvent)
         })
     }
 }
