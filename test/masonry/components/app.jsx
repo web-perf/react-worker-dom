40f8eaeb3b2f689e2d2b7516ebb180807d3b623e
amitk <amitk@wix.com>
2016-07-26 10:32:24 +0300

    Some order in the house

--- test/masonry/components/app.jsx
+++ test/masonry/components/app.jsx
@@ -6,6 +6,9 @@ const css = `
   width: 600px;
 }
 .box {
+    color: white;
+    text-align: center;
+    font-size: 18pt;
     width: 180px;
     margin: 10px;
     position: absolute;
@@ -18,6 +21,19 @@ function randInt(v) {
     return Math.floor(Math.random() * v);
 }
 
+const Masonry = React.createClass({
+    render: function () {
+        return (
+            <div {...this.props} ref="masonry">
+                {this.props.children}
+            </div>
+        );
+    },
+    componentDidMount: function () {
+        this.refs.masonry.invoke('masonry', [this.props.columns]);
+    }
+});
+
 const App = React.createClass({
     getInitialState: ()=> {
         return {
@@ -28,17 +44,15 @@ const App = React.createClass({
     },
     render: function () {
         return (
-            <div className="container" ref="masonry">
-                 <style dangerouslySetInnerHTML={{__html:css}}/>
-
-                 {this.state.items.map((h, index) => 
-                    <div className="box" style={{height:h +'px'}} key={'box'+index}>{index}</div>)}
+            <div style="min-height:50px">
+                <style dangerouslySetInnerHTML={{__html:css}}/>
+                <Masonry columns={3} className="container">
+                    {this.state.items.map((h, index) =>
+                        <div className="box" style={{height:h +'px'}} key={'box'+index}>{index}</div>)}
+                </Masonry>
             </div>
         );
-    },
-    componentDidMount: function () {
-        this.refs.masonry.invoke('masonry', [3]);
     }
-})
+});
 
-module.exports = React.createFactory(App);
+module.exports = App;
