d47ebba4714058b46c3bee5879339850e2e90c21
Parashuram <code@nparashuram.com>
2016-08-20 18:46:54 -0700

    Fixed bug in remove child nodes

--- src/worker/dom/TreeNode.js
+++ src/worker/dom/TreeNode.js
@@ -33,7 +33,7 @@ export default class TreeNode {
 
     removeChild(node) {
         let childIndex = this.children.indexOf(node);
-        if (childIndex > 0) {
+        if (childIndex >= 0) {
             this.children.splice(childIndex, 1);
             this._bridge.send(_.removeChild, this._guid, [node]);
         }
