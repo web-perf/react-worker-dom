32203e1bbeaa74a11812d6ed2df50e4a14943000
Linus Jakobsson <Linus.Jakobsson@infor.com>
2016-10-31 10:03:48 +0100

    Validate touch to contain properties.

--- src/common/api/Touch.js
+++ src/common/api/Touch.js
@@ -1,5 +1,6 @@
 export default class Touch {
     constructor(touch) {
+      if(touch) {
         this.clientX = touch.clientX;
         this.clientY = touch.clientY;
         this.identifier = touch.identifier;
@@ -7,5 +8,6 @@ export default class Touch {
         this.pageY = touch.pageY;
         this.screenX = touch.screenX;
         this.screenY = touch.screenY;
+      }
     }
 }
